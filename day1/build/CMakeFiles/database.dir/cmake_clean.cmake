bead344a2603934d656bb1889542dc385fa35dcf
erfengwelink <erfengwelink@welink.io>
2018-06-11 20:05:24 +0800

    Part 2 - World's Simplest SQL Compiler and Virtual Machine

---
+++ day1/build/CMakeFiles/database.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/database.dir/main.c.o"
+  "database.pdb"
+  "database"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
